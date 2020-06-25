340db05c5d556643234d5a74fe80c4da9630188e
Aaron G <aarongoertzen@gmail.com>
2020-06-25 16:21:41 -0400

    .

--- tests/tests.browsers.js
+++ tests/tests.browsers.js
@@ -258,18 +258,15 @@ process.on("unhandledRejection", err => {
   // Note that Node 6 does not have fs.copyFile()
   console.log("Copying browser bundle and sourcemaps...");
   const bundle = await promisify(fs.readFile)(
-    `${__dirname}/webroot/bundle.withmaps.js`
+    `${__dirname}/../build/bundle.withmaps.js`
   );
-  console.log(1);
   await promisify(fs.writeFile)(
     `${__dirname}/webroot/bundle.withmaps.js`,
     bundle
   );
-  console.log(2);
   const maps = await promisify(fs.readFile)(
     `${__dirname}/../build/bundle.withmaps.js.map`
   );
-  console.log(3);
   await promisify(fs.writeFile)(
     `${__dirname}/webroot/bundle.withmaps.js.map`,
     maps
