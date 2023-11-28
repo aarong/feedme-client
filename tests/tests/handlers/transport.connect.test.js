6cf6d5fbe075a1f79e5eacb2efca5963a001c06a
Aaron G <aarongoertzen@gmail.com>
2023-11-28 12:59:40 -0500

    test

--- tests/tests/handlers/transport.connect.test.js
+++ tests/tests/handlers/transport.connect.test.js
@@ -3,5 +3,9 @@
 describe("The transport connect event handler", () => {
   describe("invalid transport behavior", () => {});
 
-  describe("valid transport behavior", () => {});
+  describe("valid transport behavior", () => {
+    it("should do something", () => {
+      expect(1).toBe(2);
+    });
+  });
 });
